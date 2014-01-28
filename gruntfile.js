63db9222caa3583c95defe73738ce61e723a14ed
DarkPark <darkpark@pisem.net>
2014-01-28 23:43:13 +0200

    add new grunt build rules

--- gruntfile.js
+++ gruntfile.js
@@ -10,14 +10,43 @@ module.exports = function ( grunt ) {
 	grunt.initConfig({
 		project: grunt.file.readJSON('package.json'),
 
+		nodemon: {
+			run: {
+				options: {
+					nodeArgs: ['--debug'],
+					watchedFolders: ['server']
+				}
+			}
+		},
+
+		jade: {
+			dev: {
+				options: {
+					pretty: true,
+					data: {
+						debug: true,
+						title: '<%= project.name %>'
+					}
+				},
+				files: {
+					'client/pub/index.html': ['client/jade/main.jade']
+				}
+			}
+		},
+
 		browserify: {
-			options: {
-				ignore: ['crypto'],
-				debug: true
-			},
-			build: {
-				src : ['client/js/*.js', 'client/js/lib/*.js'],
-				dest: 'client/build.js'
+			dev: {
+				options: {
+					ignore: ['crypto'],
+					debug: true
+				},
+				files: {
+					'client/pub/app.js': 'client/js/main.js'
+					//src : ['client/js/*.js', 'client/js/lib/*.js'],
+					//src : ['client/js/main.js'],
+					//src : ['client/js/tst/*.js'],
+					//dest: 'client/pub/app.js'
+				}
 			}
 		},
 
@@ -26,23 +55,28 @@ module.exports = function ( grunt ) {
 				banner: '/*! <%= project.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
 			},
 			build: {
-				src : 'client/build.js',
-				dest: 'client/build.js'
+				src : 'client/pub/app.js',
+				dest: 'client/pub/app.js'
 			}
 		},
 
 		jshint: {
-			options: grunt.file.readJSON('client/.jshintrc'),
-			files: ['client/js/*.js', 'client/js/lib/*.js', '!client/js/lib/sjcl.js']
+			options: {
+				jshintrc: true
+			},
+			//options: grunt.file.readJSON('client/.jshintrc'),
+			//files: ['client/js/*.js', 'client/js/lib/*.js', '!client/js/lib/sjcl.js']
+			files: ['client/js/**/*.js', '!client/js/lib/sjcl.js']
 		},
 
 		less: {
-			options: {
-				cleancss: false
-			},
-			build: {
-				src : 'client/css/*.less',
-				dest: 'client/css/build.css'
+			dev: {
+				options: {
+					cleancss: false
+				},
+				files: {
+					'client/pub/app.css': 'client/less/main.less'
+				}
 			}
 		},
 
@@ -64,6 +98,13 @@ module.exports = function ( grunt ) {
 				files: ['<%= less.build.src %>'],
 				tasks: ['less']
 			}
+		},
+
+		concurrent: {
+			tasks: ['nodemon', 'watch'],
+			options: {
+				logConcurrentOutput: true
+			}
 		}
 	});
 
@@ -72,10 +113,14 @@ module.exports = function ( grunt ) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-jade');
 	grunt.loadNpmTasks('grunt-browserify');
+	grunt.loadNpmTasks('grunt-concurrent');
+	grunt.loadNpmTasks('grunt-nodemon');
 
 	// default task(s)
-	grunt.registerTask('default', ['browserify', 'uglify', 'less']);
+	//grunt.registerTask('default', ['browserify', 'uglify', 'less']);
+	grunt.registerTask('default', ['concurrent']);
 	grunt.registerTask('check', ['jshint']);
 
 };
