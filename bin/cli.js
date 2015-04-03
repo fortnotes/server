14b7ee3c8307d1b47dc0b11b7fac185feaeebd18
Stanislav Kalashnik <sk@infomir.eu>
2015-04-03 13:34:20 +0300

    correct config file loading and using

--- bin/cli.js
+++ bin/cli.js
@@ -2,7 +2,7 @@
 
 'use strict';
 
-var fs      = require('fs'),
+var //fs      = require('fs'),
 	path    = require('path'),
 	program = require('commander'),
 	pkgInfo = require('../package.json');
@@ -10,9 +10,11 @@ var fs      = require('fs'),
 // init
 program
 	.version(pkgInfo.version)
+	.description('Server application to process client REST API requests.\n  See the details at https://fortnotes.com/.')
+	.option('-d, --debug', 'enable verbose debug mode')
 	//.option('-t, --api-token-size <number>', 'HTTP port to listen [96]', 96)
 	//.option('-c, --config <file>', 'path to the configuration options file [~/.fortnotes/config.json]', path.join(process.env.HOME || process.env.USERPROFILE, '.fortnotes', 'config.json'))
-	.option('-c, --config <file>', 'path to the configuration options file', path.join(__dirname, '..', 'config.json'))
+	.option('-c, --config <file>', 'path to the configuration options JSON file', path.join(__dirname, '..', 'config.json'))
 	//.option('-p, --port <number>', 'HTTP port to listen [9090]', 9090)
 	//.option('-d, --db-uri <uri>', 'database connection URI', 'sqlite://' + path.join(process.env.HOME || process.env.USERPROFILE, '.fortnotes', 'data.sqlite'))
 ;
@@ -23,19 +25,21 @@ program
 program.on('--help', function () {
 	console.log('  Examples:');
 	console.log('');
-	console.log('    $ fortnotes --port 80');
-	console.log('    $ fortnotes --database "mysql://user:pass@localhost/dbname"');
+	console.log('    $ fortnotes --config "~/.fortnotes/config.json"');
 	console.log('');
 });
 
 // parse and invoke commands when defined
 program.parse(process.argv);
 
-console.log(program);
+//console.log(program);
 
+// map loaded configuration to global scope
 global.config = require(program.config);
 
-console.log(global.config);
+global.config.debug = !!program.debug;
+
+//console.log(global.config);
 
 require('../lib/main');
 
@@ -57,4 +61,4 @@ require('../lib/main');
 
 
 // public export
-module.exports = program;
+//module.exports = program;
