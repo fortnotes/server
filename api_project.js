3fce4f77bafbec324286ab7369ae3ee19bb26f19
DarkPark <darkpark@pisem.net>
2015-04-14 23:19:34 +0300

    update examples and remove keys resource

--- api_project.js
+++ api_project.js
@@ -24,7 +24,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-04-14T19:45:16.278Z",
+    "time": "2015-04-14T20:19:00.259Z",
     "url": "http://apidocjs.com",
     "version": "0.12.1"
   }
