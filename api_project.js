867327bcd135c5c11668a4bf4eb75d9464c5ad33
Stanislav Kalashnik <sk@infomir.eu>
2015-04-14 18:00:49 +0300

    init repository

---
+++ api_project.js
@@ -0,0 +1,31 @@
+define({
+  "name": "FortNotes REST API Documentation",
+  "version": "1.0.0",
+  "description": "FortNotes client-server communication protocol.",
+  "header": {
+    "title": "Intro",
+    "content": "<p>Welcome to the FortNotes REST API documentation.</p>\n<p>This interface is for communication between <a href=\"https://github.com/fortnotes/server\">FortNotes Server</a> and <a href=\"https://github.com/fortnotes/client\">FortNotes Client</a>.</p>\n"
+  },
+  "url": "https://api.fortnotes.com",
+  "sampleUrl": "http://localhost:9090",
+  "order": [
+    "Sessions",
+    "Session",
+    "Notes",
+    "Note",
+    "Tags",
+    "Users",
+    "Profile"
+  ],
+  "template": {
+    "withCompare": true,
+    "withGenerator": true
+  },
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2015-04-14T14:57:27.957Z",
+    "url": "http://apidocjs.com",
+    "version": "0.12.1"
+  }
+});
