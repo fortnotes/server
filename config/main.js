2fb9e714e9bea13db1a9d8fd09df41c8dea10e90
Stanislav Kalashnik <sk@infomir.eu>
2015-04-16 11:43:39 +0300

    adopt all current options

--- config/main.js
+++ config/main.js
@@ -1,5 +1,6 @@
 /**
- * Main server configuration.
+ * Default server configuration.
+ * Can be redefined by user config file options.
  *
  * @author DarkPark
  * @license GNU GENERAL PUBLIC LICENSE Version 3
@@ -8,19 +9,59 @@
 'use strict';
 
 module.exports = {
-	debug: true,
+	// enable verbose debug mode
+	debug: false,
 
-	httpPort: 9090,
+	// run tests and exit
+	test: false,
 
-	session: {
-		tokenSize:       96,
-		confirmCodeSize: 12,
-		confirmAttempts: 3
+	// HTTP port serving REST API requests
+	port: 9090,
+
+	// encrypted data size (notes, tags)
+	dataSize: 1048576,
+
+	// notes/tags sha512 hash size
+	hashSize: 128,
+
+	// default amount of returned records in lists of notes, sessions etc.
+	dataLimit: 20,
+
+	// maximum amount of returned records in lists
+	dataLimitMax: 200,
+
+	// generated token size in bytes
+	sessionTokenSize: 96,
+
+	// generated token confirmation code size in bytes
+	sessionConfirmCodeSize: 12,
+
+	// allowed amount of attempts to activate sessions
+	sessionConfirmAttempts: 3,
+
+	// nodemailer transport type
+	// possible values: smtp, ses, sendmail, pickup, direct
+	// http://adilapapaya.com/docs/nodemailer/#possibletransportmethods
+	mailTransportType: 'direct',
+
+	// nodemailer transport configuration
+	// http://adilapapaya.com/docs/nodemailer/#globaltransportoptions
+	mailTransportConfig: {},
+
+	// nodemailer e-mail message fields
+	// http://adilapapaya.com/docs/nodemailer/#emailmessagefields
+	mailOptions: {
+		from: 'admin@fortnotes.com',
+		subject: 'FortNotes Server notification'
 	},
 
-	database: {
-		host: '127.0.0.1',
-		port: 27017,
-		base: 'fortnotes'
-	}
+	// server creation options passed to restify package
+	// http://mcavage.me/node-restify/#creating-a-server
+	restify: {
+		name: 'FortNotes API REST Server'
+	},
+
+	// database connection options passed to node-orm2 package
+	// https://github.com/dresende/node-orm2/wiki/Connecting-to-Database
+	database: 'sqlite://./data.sqlite'
 };
