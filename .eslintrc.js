32bd9eb3f69f5eff6822713dafdcaa7101b814e2
DarkPark <darkpark@pisem.net>
2016-04-10 00:15:21 +0300

    use new eslint system

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+/**
+ * @author Stanislav Kalashnik <darkpark.main@gmail.com>
+ * @license GNU GENERAL PUBLIC LICENSE Version 3
+ */
+
+'use strict';
+
+// public
+module.exports = {
+    // base rules
+    extends: require.resolve('cjs-eslint-config/.eslintrc.js')
+};
