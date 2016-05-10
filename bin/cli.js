d945cd538634d87e4ccaca819bd55d17b202cba2
Stanislav Kalashnik <s.kalashnik@infomir.com>
2016-05-10 19:07:54 +0300

    init profile directory

--- bin/cli.js
+++ bin/cli.js
@@ -32,42 +32,43 @@ program.on('--help', function () {
 // parse and invoke commands when defined
 program.parse(process.argv);
 
-
 //console.log(program);
 
+// main path
 program.profile = program.profile ||
     path.join(process.env.XDG_CONFIG_HOME || path.join(process.env.HOME, '.config'), pkgInfo.name);
 
-debug('profile: ' + program.profile);
 
 // overwrite default options with user configuration
-if ( program.config ) {
-    // todo: replace with !path.isAbsolute(program.config) in node 0.12.*
-    if ( path.resolve(program.config) !== path.normalize(program.config) ) {
-        // correct relative path
-        program.config = path.join(__dirname, '..', program.config);
-    }
-
-    // valid file is given
-    if ( fs.existsSync(program.config) ) {
-        // get content
-        userConfig = require(program.config);
-        // config content is valid
-        if ( userConfig && typeof userConfig === 'object' ) {
-            debug('config file: %s', path.resolve(program.config));
-            // redefine only top level
-            Object.keys(userConfig).forEach(function (name) {
-                config[name] = userConfig[name];
-            });
-        }
-    }
-}
+// if ( program.config ) {
+//     // todo: replace with !path.isAbsolute(program.config) in node 0.12.*
+//     if ( path.resolve(program.config) !== path.normalize(program.config) ) {
+//         // correct relative path
+//         program.config = path.join(__dirname, '..', program.config);
+//     }
+//
+//     // valid file is given
+//     if ( fs.existsSync(program.config) ) {
+//         // get content
+//         userConfig = require(program.config);
+//         // config content is valid
+//         if ( userConfig && typeof userConfig === 'object' ) {
+//             debug('config file: %s', path.resolve(program.config));
+//             // redefine only top level
+//             Object.keys(userConfig).forEach(function (name) {
+//                 config[name] = userConfig[name];
+//             });
+//         }
+//     }
+// }
 
 // redefine some options
-config.debug = !!program.debug;
-config.test  = !!program.test;
+// config.debug = !!program.debug;
+// config.test  = !!program.test;
 
 //debug('config data: %o', config);
 
-// start app
-//require('../lib/main');
+// public
+module.exports = new (require('../lib/app'))({
+    profile: program.profile
+});
